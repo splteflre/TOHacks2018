b23706442896a63eb80d4b4c3150a3b211d9cda8
Linda <rui.lindawu@gmail.com>
2018-05-26 13:26:44 -0400

    Commiting finished piano key sounds.

--- musicKeys.js
+++ musicKeys.js
@@ -1,3 +1,24 @@
-var x = 5, y = 6;
-var z = x + y;
-console.log(z); 
+// document.getElementById('btn').onclick = function(){
+// //Getting button and listning for click
+// playAudio();
+// //Utilizing the HTML Audio/Video DOM Reference to play audio/video
+// }
+
+//to play on the key A do this(using Jquery):
+document.addEventListener('keydown', function(e) {
+  if(e.keyCode == 65){ //C
+  playAudioC();
+  }
+  else if(e.keyCode == 83){ //D
+    playAudioD();
+  }
+  else if(e.keyCode == 68){ //E
+    playAudioE();
+  }
+  else if(e.keyCode == 70){ //F
+    playAudioF();
+  }
+  else if(e.keyCode == 71){ //G
+    playAudioG();
+  }
+});
