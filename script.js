23a22aac36b0afb4dbcbb6da4778a5e82ac1c571
KarolinaXin <karolinaxin@yahoo.ca>
2018-05-26 16:43:20 -0400

    Combined Lindas stuff

--- script.js
+++ script.js
@@ -9,8 +9,44 @@ document.addEventListener('keydown', function (e) {
     if (e.keyCode == 32) {
         startConverting();
     }
-    if (e.keyCode == 65) {
-        playTrack1();
+    if (e.keyCode == 65) { //C
+        playAudioA();
+    }
+    if (e.keyCode == 87) { //D
+        playAudioBb();
+    }
+    if (e.keyCode == 83) { //E
+        playAudioB();
+    }
+    if (e.keyCode == 68) { //F
+        playAudioC();
+    }
+    if (e.keyCode == 82) { //G
+        playAudioCs();
+    }
+    if (e.keyCode == 70) {
+        playAudioD();
+    }
+    if (e.keyCode == 84) {
+        playAudioDs();
+    }
+    if (e.keyCode == 71) {
+        playAudioE();
+    }
+    if (e.keyCode == 72) {
+        playAudioF();
+    }
+    if (e.keyCode == 85) {
+        playAudioFs();
+    }
+    if (e.keyCode == 74) {
+        playAudioG();
+    }
+    if (e.keyCode == 73) {
+        playAudioGs();
+    }
+    if (e.keyCode == 75) {
+        playAudioAA();
     }
 });
 
