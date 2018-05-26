e1a78e13287f5ba1df52b286dacd561c3e414eee
Linda <rui.lindawu@gmail.com>
2018-05-26 16:28:45 -0400

    added more keys

--- musicKeys.js
+++ musicKeys.js
@@ -7,18 +7,42 @@
 //to play on the key A do this(using Jquery):
 document.addEventListener('keydown', function(e) {
   if(e.keyCode == 65){ //C
-  playAudioC();
+  playAudioA();
   }
-  else if(e.keyCode == 83){ //D
+if(e.keyCode == 87){ //D
+    playAudioBb();
+  }
+  if(e.keyCode == 83){ //E
+    playAudioB();
+  }
+  if(e.keyCode == 68){ //F
+    playAudioC();
+  }
+ if(e.keyCode == 82){ //G
+    playAudioCs();
+  }
+  if(e.keyCode == 70){
     playAudioD();
   }
-  else if(e.keyCode == 68){ //E
+  if(e.keyCode == 84){
+    playAudioDs();
+  }
+  if(e.keyCode == 71){
     playAudioE();
   }
-  else if(e.keyCode == 70){ //F
+  if(e.keyCode == 72){
     playAudioF();
   }
-  else if(e.keyCode == 71){ //G
+  if(e.keyCode == 85){
+    playAudioFs();
+  }
+  if(e.keyCode == 74){
     playAudioG();
+  }
+  if(e.keyCode == 73){
+    playAudioGs();
+  }
+  if(e.keyCode == 75){
+    playAudioAA();
   }
 });
